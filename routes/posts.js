fa0508c5d5db854367c2f89be18e58144ddfa7db
Akshat <akshatarya027@gmail.com>
2024-05-20 13:09:49 +0530

    first com

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 const authenticate = require('../middleware/authenticate');
+const x = 1
 
 router.post('/', authenticate, postController.createPost);
 router.get('/feed', authenticate, postController.getFeed);
